d4e82a176f89d18b0d1c8ebb4ab469f00aca81f5
nomad <nomvdneek@gmail.com>
2022-01-09 15:23:54 -0500

    style: form button
    Update submit query to Log in button.

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
         <form action="" method="POST">
             Username: <input type="text" name="name"><br>
             Password: <input type="text" name="email"><br>
-            <input type="submit">
+            <button type="submit">Log In</button>
         </form>
         </body>
-
     </html>
