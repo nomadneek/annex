0e9a32f1abba13a28f1d2850ac3505182d5df639
nomad <nomvdneek@gmail.com>
2022-01-12 19:41:00 -0500

    docs: form action
    Post form data to login page.

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
     <html>
         <head>
-            <title>Title of the document</title>
+            <title>Log In</title>
         </head>
         <body>
-        <form action="" method="POST">
+        <form action="includes/login.php" method="POST">
             Username: <input type="text" name="name"><br>
-            Password: <input type="text" name="email"><br>
+            Password: <input type="text" name="password"><br>
             <button type="submit">Log In</button>
         </form>
         </body>
